0dad2931f363f9aac89370827b126225b52471ed
amoerman <alexander.moerman@gmail.com>
2015-05-07 15:16:37 +0200

    Initial commit.

---
+++ ActiveDirectoryTester/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.DirectoryServices.Protocols;
+using System.Net;
+
+namespace ActiveDirectoryTester {
+  public class Program {
+    public static void Main(string[] args) {
+      string server = args[0];
+      string port = args[1];
+      string username = args[2];
+      string password = args[3];
+
+      string url = server;
+      if (!string.IsNullOrEmpty(port))
+        url = url + ":" + port;
+
+      try {
+        using (var connection = new LdapConnection(url)) {
+          var credentials = new NetworkCredential(username, password);
+          connection.AuthType = AuthType.Negotiate;
+          connection.Bind(credentials);
+        }
+        Console.WriteLine("SUCCESS");
+      }
+      catch (Exception e) {
+        Console.WriteLine("ERROR");
+        Console.WriteLine(e);
+      }
+    }
+  }
+}
